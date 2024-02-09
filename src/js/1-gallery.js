b8ba9dd07a9dd2ea2197d763a08a9eee3092cf48
mariabykova23 <144001967+mariabykova23@users.noreply.github.com>
2024-02-09 21:01:25 +0300

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -103,7 +103,7 @@ const lightbox = new SimpleLightbox(".gallery a", {
   captionDelay: 250,
   captionsPosition: 'bottom',
   animationSpeed:50,
-  sourceAttr:'dataset.source',
+  // sourceAttr:'dataset.source',
 });
 
 lightbox.on("show.simplelightbox");
